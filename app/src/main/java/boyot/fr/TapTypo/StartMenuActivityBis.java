01767f4608aa4806c8e25ad2935c5ba8d0222c97
JessicaCherruau <jessicacherruau@gmail.com>
2015-06-11 20:44:52 +0200

    add start menu with design

---
+++ app/src/main/java/boyot/fr/TapTypo/StartMenuActivityBis.java
@@ -0,0 +1,98 @@
+package boyot.fr.TapTypo;
+
+import boyot.fr.TapTypo.util.SystemUiHider;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import boyot.fr.TapTypo.R;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ *
+ * @see SystemUiHider
+ */
+public class StartMenuActivityBis extends Activity {
+    /**
+     * Whether or not the system UI should be auto-hidden after
+     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
+     */
+    private static final boolean AUTO_HIDE = true;
+
+    /**
+     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
+     * user interaction before hiding the system UI.
+     */
+    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+
+    /**
+     * If set, will toggle the system UI visibility upon interaction. Otherwise,
+     * will show the system UI visibility upon interaction.
+     */
+    private static final boolean TOGGLE_ON_CLICK = true;
+
+    /**
+     * The flags to pass to {@link SystemUiHider#getInstance}.
+     */
+    private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
+
+    /**
+     * The instance of the {@link SystemUiHider} for this activity.
+     */
+    private SystemUiHider mSystemUiHider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_start_menu);
+
+        //set the listener
+        final ImageButton btnSolo = (ImageButton) findViewById(R.id.startSolo);
+        btnSolo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), TapTypoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        final ImageButton btnCreate = (ImageButton) findViewById(R.id.createGame);
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Create a multiplayer game", Toast.LENGTH_SHORT);
+            }
+        });
+
+        final ImageButton btnJoin = (ImageButton) findViewById(R.id.joinGame);
+        btnJoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Join a multiplayer game", Toast.LENGTH_SHORT);
+            }
+        });
+    }
+
+
+
+
+
+    Handler mHideHandler = new Handler();
+    Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            mSystemUiHider.hide();
+        }
+    };
+
+}
